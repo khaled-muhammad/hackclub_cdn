8e2fb07a360f1a239d6e4e1329f98bd5f320b0ad
Khaled Muhammad <khaledmuhmmed99@gmail.com>
2025-07-21 07:09:46 +0300

    Some addons for using cookies with production backend from localhost to make it support https and also use the production backend as a reverse proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import fs from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +9,14 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
+  server: {
+    https: {
+      key: fs.readFileSync('localhost-key.pem'),
+      cert: fs.readFileSync('localhost.pem'),
+    },
+    port: 5173,
+    proxy: {
+      '/api': 'https://hackclub-cdn.khaled.hackclub.app/',
+    }
+  },
 })
