8d1a408a09467ae222298e53846f9efd9d4e0f04
Khaled Muhammad <khaledmuhmmed99@gmail.com>
2025-07-21 07:06:51 +0300

    made routing support my auth context

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,64 @@ import Landing from './routes/Landing.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from './Layout.tsx'
 import ErrorRoute from './routes/ErrorRoute.tsx'
+import AuthRoute from './routes/AuthRoute.tsx'
+import SetNewPasswordRoute from './routes/SetNewPasswordRoute.tsx'
+import { AuthProvider } from './contexts/AuthContext.tsx'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import ProtectedRoute from './components/ProtectedRoute.tsx'
+import AuthSession from './routes/AuthSession.tsx'
+import Dashboard from './routes/Dashboard.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route element={<Landing />} index />
-          <Route element={<ErrorRoute />} path="*" />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route element={<Landing />} index />
+            <Route 
+              path='/auth' 
+              element={
+                <ProtectedRoute requireAuth={false}>
+                  <AuthRoute />
+                </ProtectedRoute>
+              } 
+            />
+            <Route 
+              path='/auth/set_new_password' 
+              element={
+                <ProtectedRoute requireAuth={false}>
+                  <SetNewPasswordRoute />
+                </ProtectedRoute>
+              } 
+            />
+            <Route 
+              path='/auth/auth_session' 
+              element={
+                <ProtectedRoute requireAuth={false}>
+                  <AuthSession />
+                </ProtectedRoute>
+              } 
+            />
+            <Route element={<ErrorRoute />} path="*" />
+          </Route>
+          <Route path='/dashboard' element={<Dashboard />}>
+          </Route>
+        </Routes>
+        
+        {/* Toast notifications */}
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+      </BrowserRouter>
+    </AuthProvider>
 )
